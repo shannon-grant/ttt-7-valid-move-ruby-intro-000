8602ac67b3c8e70fe4193e9524af069f0ffeb9f5
declarative-linter-0823 <grant.shannon90@gmail.com>
2020-04-02 16:55:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,13 +8,6 @@ def valid_move?(board, index)
     end
   end
 
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
