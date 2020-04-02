af756ddf6211f7f49d31115d327a92b9d5fbac05
declarative-linter-0823 <grant.shannon90@gmail.com>
2020-04-02 16:57:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,11 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else
+    return false
   end
-
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
